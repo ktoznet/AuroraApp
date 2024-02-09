8331974f4338a878565a99441db1d0578d8afd05
Kirill Bereznev <86389034+ktoznet@users.noreply.github.com>
2024-02-09 23:18:42 +0700

    Ваше сообщение о коммите

---
+++ shared/settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "AuroraApp"
+include(":shared")
+includeBuild("convention-plugins")
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
